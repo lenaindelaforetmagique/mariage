5d95064d94a8f55c9a59b0c6b411dc9755b22c57
lenaindelaforetmagique <morin_xavier@hotmail.fr>
2020-04-28 10:55:48 +0200

    banner, la suite

--- banner.js
+++ banner.js
@@ -16,6 +16,37 @@ hslaGenerator = function(hue = 0, saturation = 0, light = 0, alpha = 1) {
 }
 
 
+shadowGenerator = function(id_, dx_, dy_, blur_) {
+  let filter = document.createElementNS(SVGNS, "filter");
+  filter.setAttributeNS(null, "id", id_);
+  filter.setAttributeNS(null, "x", "0");
+  filter.setAttributeNS(null, "y", "0");
+  filter.setAttributeNS(null, "width", "200%");
+  filter.setAttributeNS(null, "height", "200%");
+
+  let feOffset = document.createElementNS(SVGNS, "feOffset");
+  feOffset.setAttributeNS(null, "result", "offOut");
+  feOffset.setAttributeNS(null, "in", "SourceAlpha");
+  feOffset.setAttributeNS(null, "dx", dx_.toString());
+  feOffset.setAttributeNS(null, "dy", dy_.toString());
+  filter.appendChild(feOffset);
+
+  let feGaussianBlur = document.createElementNS(SVGNS, "feGaussianBlur");
+  feGaussianBlur.setAttributeNS(null, "result", "blurOut");
+  feGaussianBlur.setAttributeNS(null, "in", "offOut");
+  feGaussianBlur.setAttributeNS(null, "stdDeviation", blur_.toString());
+  filter.appendChild(feGaussianBlur);
+
+  let feBlend = document.createElementNS(SVGNS, "feBlend");
+  feBlend.setAttributeNS(null, "in", "SourceGraphic");
+  feBlend.setAttributeNS(null, "in2", "blurOut");
+  feBlend.setAttributeNS(null, "mode", "normal");
+  filter.appendChild(feBlend);
+
+  return filter;
+}
+
+
 class Banner {
   constructor() {
     this.container = document.getElementById("header");
@@ -26,6 +57,8 @@ class Banner {
     this.frontImageURL = "bann2-front.png";
     this.scrollingText = "Xavier & Marie-Astrid";
     this.dateText = "26.09.2020";
+
+    this.scrollSpeed = -0.04;
 
     this.init();
     // this.addEvents();
@@ -43,35 +76,12 @@ class Banner {
 
     let defs = document.createElementNS(SVGNS, "defs");
 
-    // Filter ombre
-    let filter = document.createElementNS(SVGNS, "filter");
-    filter.setAttributeNS(null, "id", "shadow");
-    filter.setAttributeNS(null, "x", "0");
-    filter.setAttributeNS(null, "y", "0");
-    filter.setAttributeNS(null, "width", "200%");
-    filter.setAttributeNS(null, "height", "200%");
+    // -- Filter ombre1
+    defs.appendChild(shadowGenerator("shadow1", 1, 1, 0));
+    defs.appendChild(shadowGenerator("shadow2", 0, 0, 1));
+    // -- Filter ombre1
 
-    let feOffset = document.createElementNS(SVGNS, "feOffset");
-    feOffset.setAttributeNS(null, "result", "offOut");
-    feOffset.setAttributeNS(null, "in", "SourceAlpha");
-    feOffset.setAttributeNS(null, "dx", "1");
-    feOffset.setAttributeNS(null, "dy", "1");
-    filter.appendChild(feOffset);
-
-    let feGaussianBlur = document.createElementNS(SVGNS, "feGaussianBlur");
-    feGaussianBlur.setAttributeNS(null, "result", "blurOut");
-    feGaussianBlur.setAttributeNS(null, "in", "offOut");
-    feGaussianBlur.setAttributeNS(null, "stdDeviation", "0");
-    filter.appendChild(feGaussianBlur);
-
-    let feBlend = document.createElementNS(SVGNS, "feBlend");
-    feBlend.setAttributeNS(null, "in", "SourceGraphic");
-    feBlend.setAttributeNS(null, "in2", "blurOut");
-    feBlend.setAttributeNS(null, "mode", "normal");
-    filter.appendChild(feBlend);
-    defs.appendChild(filter);
-
-    // fill dégradé
+    // -- fill dégradé
     let linearGradient = document.createElementNS(SVGNS, "linearGradient");
     linearGradient.setAttributeNS(null, "id", "gradient");
     linearGradient.setAttributeNS(null, "x1", "0%");
@@ -88,8 +98,8 @@ class Banner {
     stop.setAttributeNS(null, "offset", "100%");
     stop.setAttributeNS(null, "style", "stop-color:rgba(255,255,255,1);stop-opacity:1");
     linearGradient.appendChild(stop);
-
     defs.appendChild(linearGradient);
+    // -- fill dégradé
 
     this.dom.appendChild(defs);
 
@@ -98,26 +108,27 @@ class Banner {
 
     let img = document.createElementNS(SVGNS, "image");
     // img.setAttributeNS(null, "id", "shipView");
-    // img.setAttributeNS(null, "x", 0);
-    // img.setAttributeNS(null, "y", 0);
-    // img.setAttributeNS(null, "width", 54);
-    // img.setAttributeNS(null, "height", 130);
+    img.setAttributeNS(null, "x", 0);
+    img.setAttributeNS(null, "y", 0);
+    img.setAttributeNS(null, "width", 800);
+    img.setAttributeNS(null, "height", 250);
     img.setAttributeNS(attrNS, attr, this.backImageURL);
     this.dom.appendChild(img);
 
     this.text = document.createElementNS(SVGNS, "text");
     this.text.innerHTML = this.scrollingText;
-    this.text.setAttributeNS(null, "text-height", "70px");
+    this.text.setAttributeNS(null, "font-size", "70px");
     this.text.setAttributeNS(null, "y", "145");
     this.text.setAttributeNS(null, "x", this.xScroll);
     this.text.setAttributeNS(null, "fill", "#FFFFFF");
-    this.text.setAttributeNS(null, "filter", "url(#shadow)")
+    this.text.setAttributeNS(null, "filter", "url(#shadow2)")
+
 
     this.dom.appendChild(this.text);
 
     this.text2 = document.createElementNS(SVGNS, "text");
     this.text2.innerHTML = this.scrollingText;
-    this.text2.setAttributeNS(null, "text-height", "70px");
+    this.text2.setAttributeNS(null, "font-size", "70px");
     this.text2.setAttributeNS(null, "y", "-150");
     this.text2.setAttributeNS(null, "x", this.xScroll);
     this.text2.setAttributeNS(null, "transform", "scale(1 -1)")
@@ -125,30 +136,49 @@ class Banner {
 
     this.dom.appendChild(this.text2);
 
+
+    let date = document.createElementNS(SVGNS, "text");
+    date.innerHTML = this.dateText;
+    date.setAttributeNS(null, "font-size", "40px");
+    date.setAttributeNS(null, "y", "205");
+    date.setAttributeNS(null, "x", "670");
+    date.setAttributeNS(null, "fill", "#FFFFFF");
+    date.setAttributeNS(null, "filter", "url(#shadow2)")
+    this.dom.appendChild(date);
+
     img = document.createElementNS(SVGNS, "image");
+    img.setAttributeNS(null, "x", 0);
+    img.setAttributeNS(null, "y", 0);
+    img.setAttributeNS(null, "width", 800);
+    img.setAttributeNS(null, "height", 250);
     img.setAttributeNS(attrNS, attr, this.frontImageURL);
+
     this.dom.appendChild(img);
 
     // img.setAttributeNS(url)
 
-    this.updateDom();
+    // this.updateDom();
   }
 
   refresh() {
     let now = Date.now();
     if (now - this.lastUpdate > 20) {
+      this.updateDom(now - this.lastUpdate);
       this.lastUpdate = now;
-      this.updateDom();
     }
   }
 
-  updateDom() {
-    this.xScroll -= 2;
+  updateDom(dt) {
+    this.xScroll += dt * this.scrollSpeed;
+    // console.log(dt);
     let bbox = this.text.getBBox();
     let width = bbox.width;
     let height = bbox.height;
-    if (this.xScroll < -width) {
-      this.xScroll += width * 2 + 800;
+
+    if (this.xScroll + width < 0) {
+      this.xScroll += 800 + width * 1.5;
+    } else if (this.xScroll > 800) {
+      this.xScroll -= 800 + width * 1.5;
     }
     this.text.setAttributeNS(null, "x", this.xScroll);
     this.text2.setAttributeNS(null, "x", this.xScroll);
